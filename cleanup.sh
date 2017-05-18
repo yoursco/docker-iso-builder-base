43b5e2c55292f03d83cdaed05cf3d9c889ab62b4
Erick Diaz <erick@yours.co>
2017-05-18 16:43:49 -0600

    removing cleanup commands for debugging

--- cleanup.sh
+++ cleanup.sh
@@ -4,7 +4,7 @@
 set -e
 
 echo "cleaning up apt"
-apt-get clean -y
-apt-get purge build-essential -y
-apt-get autoremove -y
-rm -Rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
+#apt-get clean -y
+#apt-get purge build-essential -y
+# apt-get autoremove -y
+#rm -Rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
